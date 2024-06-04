2412e6116c5196d090306bc1f4c23c53e3f2881a
Shemaiah Chris dariya <131563232+Shemaia@users.noreply.github.com>
2024-06-04 23:20:33 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+     Computer hp = new Computer(new Ram(), new Processor(),new Storage());
+     System.out.println(hp);
+    
+    }
+}
